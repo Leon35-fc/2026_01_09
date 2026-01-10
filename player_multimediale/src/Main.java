6b929cfb1a7094822071cfa3d55487d501a029a6
Leon35-fc <leon.f35@alice.it>
2026-01-10 04:19:31 +0100

    Implmenentation of elementoMultimediale Class

--- player_multimediale/src/Main.java
+++ player_multimediale/src/Main.java
@@ -1,8 +1,18 @@
+import entities.elementoMultimediale;
+
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Il programma è partito.");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Inserisci il nome del file.");
+        String title = scn.nextLine();
+        System.out.println("Inserisci la durata. (se è un'immagine premi \"INVIO\".)");
+        int duration = scn.nextInt();
 
+        elementoMultimediale el1 = new elementoMultimediale(title, duration);
+        System.out.println(el1.getTitle());
+        System.out.println(el1.getDuration());
         }
     }
