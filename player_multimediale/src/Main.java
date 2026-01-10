b7922205205fa017be2db44ec025a14d3010227e
Leon35-fc <leon.f35@alice.it>
2026-01-10 05:12:07 +0100

    Update Main.java

--- player_multimediale/src/Main.java
+++ player_multimediale/src/Main.java
@@ -6,13 +6,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Inserisci il nome del file.");
-        String title = scn.nextLine();
-        System.out.println("Inserisci la durata. (se è un'immagine premi \"INVIO\".)");
-        int duration = scn.nextInt();
+        System.out.println("Aggiungi elementi all'elenco");
 
-        elementoMultimediale el1 = new elementoMultimediale(title, duration);
-        System.out.println(el1.getTitle());
-        System.out.println(el1.getDuration());
+        elementoMultimediale[] elements = new elementoMultimediale[6];
+
+        for (byte i = 1; i < 6; i++){
+            System.out.println("Inserisci il nome del file.");
+            String title = scn.nextLine();
+            System.out.println("Inserisci la durata del file. (Se è un'immagine premi 0.)");
+            int duration = scn.nextInt();
+            scn.nextLine();
+            elements[i] = new elementoMultimediale(title, duration);
+        }
+//        elementoMultimediale el1 = new elementoMultimediale(title, duration);
+        System.out.println(elements[1].getTitle());
+        System.out.println(elements[1].getDuration());
         }
     }
